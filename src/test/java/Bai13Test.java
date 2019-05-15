c18af4172c73d8f6aa2284910adf94cd7e2a64a6
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-15 17:09:31 +0700

    update Bai13

--- src/test/java/Bai13Test.java
+++ src/test/java/Bai13Test.java
@@ -6,4 +6,9 @@ public class Bai13Test {
     public void test_Bai13() {
         assertEquals(new Bai13().getResurt(), "2.718282");
     }
+    @Test
+    public void test_layGiaThua() {
+        assertEquals(new Bai13().layGiaThua(3), 6);
+        assertEquals(new Bai13().layGiaThua(1), 1);
+    }
 }
