e21a65c78b5eb3833b6b5a533e09e3f4c05c2084
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-15 11:05:06 +0700

    test UCLN

--- src/main/java/UCLN_BCNN.java
+++ src/main/java/UCLN_BCNN.java
@@ -22,35 +22,4 @@ public class UCLN_BCNN {
         }
         return a;
     }
-
-    public int find_BCNN(int a, int b) {
-//        int index = 0;
-//        boolean check = false;
-//        if(a == b ){
-//            return a;
-//        }
-//        if(a > b) {
-//            index = a;
-//            check = false;
-//        }
-//        else{
-//            index = b;
-//            check = true;
-//        }
-//        int jump = 1;
-//        for(int i = index; ; i *= jump ) {
-//            if(check == false) {
-//                if(i % b == 0) {
-//                    return i;
-//                }
-//            }
-//            else {
-//                if(i % a == 0)
-//                {
-//                    return i;
-//                }
-//            }
-//            jump++;
-        return 1;
-    }
 }
--- src/test/java/UCLN_BCNNTEST.java
+++ src/test/java/UCLN_BCNNTEST.java
@@ -14,13 +14,4 @@ public class UCLN_BCNNTEST {
         assertEquals(new UCLN_BCNN().find_UCLN(6,6), 6);
         assertEquals(new UCLN_BCNN().find_UCLN(5,6), 1);
     }
-
-    @Test
-    public void test_find_BCNN(){
-//        assertEquals(new UCLN_BCNN().find_BCNN(6,9), 18);
-//        assertEquals(new UCLN_BCNN().find_BCNN(9,6), 18);
-//        assertEquals(new UCLN_BCNN().find_BCNN(6,6), 6);
-//        assertEquals(new UCLN_BCNN().find_BCNN(6,12), 12);
-        assertEquals(new UCLN_BCNN().find_BCNN(6,9), 1);
-    }
 }
