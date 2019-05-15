dd498b0a24a973411b5207e68425e44432288f5b
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-15 15:49:30 +0700

    update func getPi

--- src/main/java/Exercise9.java
+++ src/main/java/Exercise9.java
@@ -1,3 +1,28 @@
+import java.text.DecimalFormat;
+
 public class Exercise9 {
 
+    public String getPi() {
+        DecimalFormat df = new DecimalFormat("#.0000");
+        double pi = 4;
+        double old = pi;
+       for (int i = 1; ; ++i)
+       {
+           if(i % 2 != 0){
+               pi = pi - (4/(2*i + 1.0));
+           }
+           else{
+               pi = pi + 4/(2*i + 1.0);
+           }
+           double ne = 4/(2*i + 1.0);
+
+           if(old - ne <= 0.0001)
+           {
+               break;
+           }
+           old = ne;
+       }
+
+       return df.format(pi);
+    }
 }
--- src/test/java/Exercise9Test.java
+++ src/test/java/Exercise9Test.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 public class Exercise9Test {
+    @Test
+    public void test_getPi() {
+        assertEquals(new Exercise9().getPi(), "3.1486");
+    }
+
 }
