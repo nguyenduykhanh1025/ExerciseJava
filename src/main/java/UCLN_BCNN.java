a235911d2c206fddb51eaadce96a1de5d3e06443
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-15 11:31:16 +0700

    upadate func UCLN_BCNN

--- src/main/java/UCLN_BCNN.java
+++ src/main/java/UCLN_BCNN.java
@@ -1,4 +1,20 @@
 public class UCLN_BCNN {
+
+    public int find_UCLN(int a, int b)
+    {
+        if (a*b == 0) {
+            return a + b;
+        }
+
+        if (a == b){
+           return a;
+        }
+        if(a > b){
+            return find_UCLN(a - b, b);
+        }
+        return find_UCLN(a, b - a);
+    }
+
     public int find_BCNN(int a, int b) {
         int index = 0;
         boolean check = false;
--- src/test/java/UCLN_BCNNTEST.java
+++ src/test/java/UCLN_BCNNTEST.java
@@ -6,6 +6,15 @@ import static org.junit.Assert.assertEquals;
 public class UCLN_BCNNTEST {
 
     @Test
+    public  void  test_find_UCLN() {
+        assertEquals(new UCLN_BCNN().find_UCLN(0, 4), 4);
+        assertEquals(new UCLN_BCNN().find_UCLN(4,0),4);
+        assertEquals(new UCLN_BCNN().find_UCLN(4,4), 4);
+        assertEquals(new UCLN_BCNN().find_UCLN(36,60),12);
+        assertEquals(new UCLN_BCNN().find_UCLN(60,36),12);
+    }
+
+    @Test
     public void test_find_BCNN(){
         assertEquals(new UCLN_BCNN().find_BCNN(6,9), 18);
         assertEquals(new UCLN_BCNN().find_BCNN(9,6), 18);
