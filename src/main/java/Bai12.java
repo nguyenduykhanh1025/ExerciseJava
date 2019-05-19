fd82ab61a2744ed14b5d5e0e47fabf242426bead
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-19 15:09:48 +0700

    upadate

--- src/main/java/Bai12.java
+++ src/main/java/Bai12.java
@@ -1,26 +1,25 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
-import java.text.DecimalFormat;
+import java.math.BigInteger;
 
 public class Bai12 {
 
-    public int layGiaThua(int n) {
-
-        if(n <= 1) {
-            return 1;
+    public BigInteger layGiaThua(int n) {
+        BigInteger p = new BigInteger("1");
+        if (n <= 1) {
+            return p;
         }
-        int p = 1;
-        for(int i = 2; i<= n; ++i) {
-            p*=i;
+        for (int i = 2; i <= n; ++i) {
+            BigInteger index = new BigInteger(String.format("%d", i));
+            p = p.multiply(index);
         }
         return p;
     }
-    public String getResurt(int n) {
-        DecimalFormat df = new DecimalFormat("#.00000");
+
+    public String getResult(int n) {
+        String str;
         double p = 0.0;
-       for(int i = 0; i<=n; ++i) {
-           p += (1.0/layGiaThua(i));
-       }
-       return df.format(p);
+        for (int i = 0; i <= n; ++i) {
+            p += 1.0 / layGiaThua(i).longValue();
+        }
+        return String.format("%.5f", p);
     }
 }
--- src/test/java/Bai12Test.java
+++ src/test/java/Bai12Test.java
@@ -1,10 +1,20 @@
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.Assert.assertEquals;
+
 public class Bai12Test {
     @Test
-    public void test_getResurt() {
-        assertEquals(new Bai12().getResurt(3), "2.66667");
+    public void test_getResult() {
+
+        assertEquals(new Bai12().getResult(3), "2.66667");
+        assertEquals(new Bai12().getResult(65), "2.71828");
     }
 
+    @Test
+    public void test_layGiaThua() {
+
+        assertEquals(new Bai12().layGiaThua(65), new BigInteger("8247650592082470666723170306785496252186258551345437492922123134388955774976000000000000000"));
+    }
 }
