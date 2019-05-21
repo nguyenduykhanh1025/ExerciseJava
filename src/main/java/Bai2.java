dacaecf9ea39483b73022678fcac38cc72c0941d
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-21 11:02:41 +0700

    change in func findSumOfNumber

--- src/main/java/Bai2.java
+++ src/main/java/Bai2.java
@@ -4,8 +4,9 @@ public class Bai2 {
         int lengthNumber = (int) Math.log10(number) + 1;
         int sum = 0;
         for (int i = lengthNumber - 1; i > 0; --i) {
-            sum += (int) (number / Math.pow(10, i));
-            number = number % (int) Math.pow(10, i);
+            int valueDenominator = (int)Math.pow(10,i);
+            sum += (int) (number / valueDenominator);
+            number = number % (int) valueDenominator;
         }
         sum += number;
         return sum;
