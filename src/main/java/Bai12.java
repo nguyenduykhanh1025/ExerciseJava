5a44ddee28e4f7179b64c74c1744aa95dd5149c2
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-21 09:48:39 +0700

    update fun gitSUmS

--- src/main/java/Bai12.java
+++ src/main/java/Bai12.java
@@ -1,25 +1,17 @@
-import java.math.BigInteger;
 
 public class Bai12 {
 
-    public BigInteger layGiaThua(int n) {
-        BigInteger p = new BigInteger("1");
-        if (n <= 1) {
-            return p;
-        }
-        for (int i = 2; i <= n; ++i) {
-            BigInteger index = new BigInteger(String.format("%d", i));
-            p = p.multiply(index);
-        }
-        return p;
-    }
-
-    public String getResult(int n) {
-        String str;
-        double p = 0.0;
-        for (int i = 0; i <= n; ++i) {
-            p += 1.0 / layGiaThua(i).longValue();
-        }
-        return String.format("%.5f", p);
+    public Double getSumS(int n) {
+        double result = 1.0, resultOld = 0;
+        int i = 0, pre = 1;
+        do {
+            if (i != 0) {
+                resultOld = result;
+            }
+            i++;
+            pre = pre * i;
+            result += Math.pow(pre, -1.0);
+        } while (i <= n);
+        return result;
     }
 }
--- src/test/java/Bai12Test.java
+++ src/test/java/Bai12Test.java
@@ -7,14 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class Bai12Test {
     @Test
     public void test_getResult() {
-
-        assertEquals(new Bai12().getResult(3), "2.66667");
-        assertEquals(new Bai12().getResult(65), "2.71828");
-    }
-
-    @Test
-    public void test_layGiaThua() {
-
-        assertEquals(new Bai12().layGiaThua(65), new BigInteger("8247650592082470666723170306785496252186258551345437492922123134388955774976000000000000000"));
+        assertEquals(new Bai12().getSumS(3), 2.708333333333333, 0.000001);
+        assertEquals(new Bai12().getSumS(15), 2.718281830583527, 0.000001);
     }
 }
