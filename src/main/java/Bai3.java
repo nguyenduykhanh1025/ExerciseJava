dae0aeb8f8c015af9d15a76403f75c105bad6279
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-27 14:09:34 +0700

    change return value in fun

--- src/main/java/Bai3.java
+++ src/main/java/Bai3.java
@@ -2,7 +2,10 @@ public class Bai3 {
     public String phanTichThuaSoNguyenTo(int number){
         StringBuffer strResult = new StringBuffer("");
         if(number == 1){
-            return "";
+            return "1";
+        }
+        if(number == 0){
+            return "0";
         }
         int jumbNumber = 2;
         while (number != 1){
@@ -16,5 +19,7 @@ public class Bai3 {
         }
         int lenString = strResult.length();
         return strResult.delete(lenString - 1, lenString).toString();
+    }
+    public static void main(String []ar){
     }
 }
--- src/test/java/Bai3Test.java
+++ src/test/java/Bai3Test.java
@@ -6,6 +6,7 @@ public class Bai3Test {
     @Test
     public void test_phanTichThuaSoNguyenTo(){
         assertEquals(new Bai3().phanTichThuaSoNguyenTo(600), "2*2*2*3*5*5");
-        assertEquals(new Bai3().phanTichThuaSoNguyenTo(1), "");
+        assertEquals(new Bai3().phanTichThuaSoNguyenTo(1), "1");
+        assertEquals(new Bai3().phanTichThuaSoNguyenTo(0), "0");
     }
 }
