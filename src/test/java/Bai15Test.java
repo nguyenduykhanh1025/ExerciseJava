59da63eb7940535d26235a4e409fb725b2d50a05
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 16:29:07 +0700

    change in func Bai5Test

--- src/test/java/Bai15Test.java
+++ src/test/java/Bai15Test.java
@@ -7,6 +7,8 @@ public class Bai15Test {
     public void test_findinfomationOfTriangle(){
         assertEquals(new Bai15().findInfomationOfTriangle(3,4,6),"13.0 90.49861877399013 thuong");
         assertEquals(new Bai15().findInfomationOfTriangle(3,1,1),"khong phai la tam giac");
+        assertEquals(new Bai15().findInfomationOfTriangle(1,3,1),"khong phai la tam giac");
+        assertEquals(new Bai15().findInfomationOfTriangle(1,1,3),"khong phai la tam giac");
         assertEquals(new Bai15().findInfomationOfTriangle(3,4,4),"11.0 65.66582063752801 can");
         assertEquals(new Bai15().findInfomationOfTriangle(4,4,3),"11.0 65.66582063752801 can");
         assertEquals(new Bai15().findInfomationOfTriangle(4,3,4),"11.0 65.66582063752801 can");
