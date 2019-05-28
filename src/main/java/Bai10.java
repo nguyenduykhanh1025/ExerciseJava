71f5c737eed5d08aa4f84d941fba963aef98d958
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 14:14:39 +0700

    add func findFn in class Bai10

--- src/main/java/Bai10.java
+++ src/main/java/Bai10.java
@@ -1,9 +1,13 @@
 public class Bai10 {
 
+    public double findFn(int i) {
+        return 4.0 / (i * (i + 1) * (i + 2));
+    }
+
     public double findPi() {
         double pi = 3;
         int i = 2;
-        double valueNext = 4.0 / (i * (i + 1) * (i + 2));
+        double valueNext = findFn(i);
         double distance = 3 - valueNext;
         while (distance >= 0.00001) {
             if (i % 4 != 0) {
@@ -12,10 +16,10 @@ public class Bai10 {
                 pi -= valueNext;
             }
             i += 2;
-            distance = valueNext - 4.0 / (i * (i + 1) * (i + 2));
-            valueNext = 4.0 / (i * (i + 1) * (i + 2));
+            distance = valueNext - findFn(i);
+            valueNext = findFn(i);
         }
 
-        return (double) Math.round(pi * 100000) / 100000;
+        return pi;
     }
 }
--- src/test/java/Bai10Test.java
+++ src/test/java/Bai10Test.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertEquals;
 public class Bai10Test {
     @Test
     public void test_findPi(){
-        assertEquals(new Bai10().findPi(), 3.14162, 0.0001);
+        assertEquals(new Bai10().findPi(), 3.1416238066678384, 0.0001);
+    }
+    @Test
+    public void test_findFn(){
+        assertEquals(new Bai10().findFn(1),0.6666666666666666,0.0001);
     }
 }
