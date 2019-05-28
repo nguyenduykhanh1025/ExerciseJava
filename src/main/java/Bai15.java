0e47d36c71fd1ec888b481ae9d8e6a3da36eb442
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 15:33:55 +0700

    update first class Bai15

---
+++ src/main/java/Bai15.java
@@ -0,0 +1,24 @@
+public class Bai15 {
+
+    public String findInfomationOfTriangle(int a, int b, int c) {
+        StringBuffer strResult = new StringBuffer("");
+        if ((a + b) < c || (b + c) < a || (a + c) < b) {
+            return "khong phai la tam giac";
+        } else {
+            double p = a + b + c;
+            strResult.append(p + " ");
+
+            double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            strResult.append(S + " ");
+
+            if (Math.sqrt(a * a + b * b) == c || Math.sqrt(b * b + c * c) == a || Math.sqrt(a * a + c * c) == b) {
+                strResult.append("vuong");
+            } else if (a == b || a == c || b == c) {
+                strResult.append("can");
+            } else {
+                strResult.append("thuong");
+            }
+        }
+        return strResult.toString();
+    }
+}
