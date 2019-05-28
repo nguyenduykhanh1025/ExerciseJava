d7bc1b9c842b6db860837f427d672d7d0eb5c829
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 13:49:27 +0700

    add func findFn in class Bai9

--- src/main/java/Bai9.java
+++ src/main/java/Bai9.java
@@ -1,19 +1,22 @@
 public class Bai9 {
 
+    public double findFn(int i) {
+        return 4.0 / (2 * i + 1);
+    }
+
     public double findPi() {
         double pi = 4;
         int i = 1;
-        double distanceOfPi = 4 - 4.0 / (2 * i + 1);
+        double distanceOfPi = findFn(0) - findFn(i);
         while (distanceOfPi >= 0.0001) {
-            if(i % 2 != 0){
-                pi -= 4.0/(2*i + 1);
-            }
-            else{
-                pi += 4.0/(2*i + 1);
+            if (i % 2 != 0) {
+                pi -= 4.0 / (2 * i + 1);
+            } else {
+                pi += 4.0 / (2 * i + 1);
             }
             i++;
-            distanceOfPi = 4.0/(2*(i - 1) + 1) - 4.0/(2*i + 1);
+            distanceOfPi = findFn(i - 1) - findFn(i);
         }
-        return (double)Math.round(pi*10000)/10000;
+        return (double) Math.round(pi * 10000) / 10000;
     }
 }
--- src/test/java/Bai9Test.java
+++ src/test/java/Bai9Test.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.assertEquals;
 
 public class Bai9Test {
     @Test
-    public void test_findPi(){
+    public void test_findPi() {
         assertEquals(new Bai9().findPi(), 3.1346, 0.0001);
+    }
+
+    public void test_findFn() {
+        assertEquals(new Bai9().findFn(1), 1.3333333333333333, 0.0001);
+        assertEquals(new Bai9().findFn(0), 4.0, .0001);
     }
 }
