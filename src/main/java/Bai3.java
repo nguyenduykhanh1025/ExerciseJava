e98c9db1b099eca163bf9b893c09d6f7f1c1e8b2
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 13:41:31 +0700

    change in Class Bai3

--- src/main/java/Bai3.java
+++ src/main/java/Bai3.java
@@ -1,23 +1,21 @@
 public class Bai3 {
-    public String phanTichThuaSoNguyenTo(int number){
+    public String phanTichThuaSoNguyenTo(int number) {
         StringBuffer strResult = new StringBuffer("");
-        if(number == 1){
-            return "1";
-        }
-        if(number == 0){
-            return "0";
+        if (number <= 2) {
+            return "" + number;
         }
         int jumbNumber = 2;
-        while (number != 1){
-            if(number % jumbNumber == 0){
-                strResult.append(jumbNumber + "*");
-                number/=jumbNumber;
-            }
-            else {
+        while (number != 1) {
+            if (number % jumbNumber == 0) {
+                strResult.append(jumbNumber);
+                number /= jumbNumber;
+                if (number != 1) {
+                    strResult.append("*");
+                }
+            } else {
                 jumbNumber++;
             }
         }
-        int lenString = strResult.length();
-        return strResult.delete(lenString - 1, lenString).toString();
+        return strResult.toString();
     }
 }
