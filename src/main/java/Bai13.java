d8be4325e03b25c7459ba9589fa5c8baee00496e
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 14:44:13 +0700

    update again

--- src/main/java/Bai13.java
+++ src/main/java/Bai13.java
@@ -1,20 +1,13 @@
 public class Bai13 {
-    public int findFactorial(int n) {
-        int factorial = 1;
-        for (int i = 2; i <= n; ++i) {
-            factorial *= i;
-        }
-        return factorial;
-    }
 
     public double findE() {
-        double valueOfENext, e = 0;
-        int i = 1;
+        double valueOfENext, e = 1;
+        int i = 1, factorial = 1;
         do {
-            e += 1.0 / findFactorial(i);
-            valueOfENext = e + 1.0 / findFactorial(i + 1);
-            i++;
+            e += 1.0 / factorial;
+            factorial *= ++i;
+            valueOfENext = e + 1.0 / factorial;
         } while (valueOfENext - e > 0.00001);
-        return (double) Math.round(e * 100000) / 100000;
+        return e;
     }
 }
