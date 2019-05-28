720f94401f736f7902d2af46bf268b223a7c317b
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 14:31:54 +0700

    remove func findFatorial in class Bai12

--- src/main/java/Bai12.java
+++ src/main/java/Bai12.java
@@ -1,18 +1,13 @@
 public class Bai12 {
 
-    public int findFactorial(int n) {
+    public Double findSumFollowFn(int n) {
+        double sum = 1;
         int factorial = 1;
-        for (int i = 2; i <= n; ++i) {
+        for (int i = 1; i <= n; ++i) {
             factorial *= i;
+            sum += 1.0 / factorial;
         }
-        return factorial;
+        return sum;
     }
 
-    public Double findSumFollowFn(int n) {
-        double sum = 0;
-        for (int i = 0; i <= n; ++i) {
-            sum += 1.0 / findFactorial(i);
-        }
-        return (double) Math.round(sum * 10000) / 10000;
-    }
 }
--- src/test/java/Bai12Test.java
+++ src/test/java/Bai12Test.java
@@ -4,13 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 public class Bai12Test {
     @Test
-    public void test_findFactorial(){
-        assertEquals(new Bai12().findFactorial(10), 3628800);
-        assertEquals(new Bai12().findFactorial(0), 1);
-    }
-    @Test
-    public void test_findSumFollowFn(){
-        assertEquals(new Bai12().findSumFollowFn(10), 2.7183, 0.001 );
-        assertEquals(new Bai12().findSumFollowFn(0), 1.0,0.001);
+    public void test_findSumFollowFn() {
+        assertEquals(new Bai12().findSumFollowFn(10), 2.7183, 0.001);
+        assertEquals(new Bai12().findSumFollowFn(0), 1.0, 0.001);
     }
 }
