4a6cb858f3ace6ab0fe36625a95d39d206f282c4
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-28 13:34:12 +0700

    change in class Bai4

--- src/main/java/Bai4.java
+++ src/main/java/Bai4.java
@@ -1,28 +1,25 @@
 public class Bai4 {
     public String phanTichThuaSoNguyenTo(int number) {
         StringBuffer strResult = new StringBuffer("");
-        if (number == 1) {
-            return "1";
-        }
-        if (number == 0) {
-            return "0";
+        if (number <= 2) {
+            return "" + number;
         }
         int jumpNumber = 2, countOfJumpNumber = 0;
         while (number != 1) {
             if (number % jumpNumber == 0) {
                 countOfJumpNumber++;
                 number /= jumpNumber;
-
-                // this is final that number % jumpNumber == 0
                 if (number % jumpNumber != 0) {
-                    strResult.append(jumpNumber + "^" + countOfJumpNumber + "+");
+                    strResult.append(jumpNumber + "^" + countOfJumpNumber);
+                    if (number != 1) {
+                        strResult.append("+");
+                    }
                     countOfJumpNumber = 0;
                 }
             } else {
                 jumpNumber++;
             }
         }
-        int lenString = strResult.length();
-        return strResult.delete(lenString - 1, lenString).toString();
+        return strResult.toString();
     }
 }
