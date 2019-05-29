9e30ae1d229859786d657fd6841ccd27f0578297
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-05-29 14:06:55 +0700

    change in func findSum class Bai2

--- src/main/java/Bai2.java
+++ src/main/java/Bai2.java
@@ -3,7 +3,7 @@ public class Bai2 {
     public int findSumOfNumber(int number) {
         int lengthNumber = (int) Math.log10(number) + 1;
         int sum = 0;
-        for (int i = 1; i <= lengthNumber; ++i) {
+        while (number != 0){
             sum += number % 10;
             number /= 10;
         }
