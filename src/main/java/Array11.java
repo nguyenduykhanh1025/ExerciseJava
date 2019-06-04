f48a3c4045469bc8e5614ff50be95a9607647346
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-04 22:54:30 +0700

    up test

--- src/main/java/Array11.java
+++ src/main/java/Array11.java
@@ -1,26 +1,24 @@
 public class Array11 {
+
     public int[] getArrayReverseEvenOdd(int[] arr, int n) {
+
         int indexOdd = n - 1, indexEven = n - 1;
+
         for (int i = 0; i <= n / 2; ++i) {
-            if (arr[i] % 2 == 0) {
-                for (int j = indexEven; j >= n / 2; --j) {
-                    if (arr[j] % 2 == 0) {
-                        int temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                        indexEven = j - 1;
-                        break;
-                    }
+            for (int j = (arr[i] % 2 == 0) ? indexEven : indexOdd; j >= 0; --j) {
+                if (arr[i] % 2 == 0 && arr[j] % 2 == 0 && j <= indexEven) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    indexEven = j - 1;
+                    break;
                 }
-            } else {
-                for (int j = indexOdd; j >= n / 2; --j) {
-                    if (arr[j] % 2 != 0) {
-                        int temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                        indexOdd = j - 1;
-                        break;
-                    }
+                else if (arr[i] % 2 != 0 && arr[j] % 2 != 0 && j <= indexOdd) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    indexOdd = j - 1;
+                    break;
                 }
             }
         }
