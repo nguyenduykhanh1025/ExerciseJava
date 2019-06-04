49237878b7231eef4aee2b1d3b2a32d2fd8f36b0
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-04 14:54:01 +0700

    up

--- src/test/java/Array2Test.java
+++ src/test/java/Array2Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class Array2Test {
     @Test
     public void test_sumOfThreeNumberMax(){
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {1,2,3,4,5};
         int n = 5;
         assertEquals(new Array2().sumOfThreeNumberMax(arr,n),12);
     }
