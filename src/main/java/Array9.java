c4e34feefb58518d0752b9af1f5bd48c5ea89d33
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-04 17:09:13 +0700

    udate

--- src/main/java/Array9.java
+++ src/main/java/Array9.java
@@ -1,24 +1,17 @@
 public class Array9 {
 
-    public int binarySearch(int []arr, int start, int stop, int number){
+    public int binarySearch(int[] arr, int start, int stop, int number) {
         int mid = (start + stop) / 2;
-        if(arr[mid] == number){
+        if (arr[mid] == number) {
             return mid;
         }
-        if(start == stop){
+        if (start == stop) {
             return -1;
         }
-        if(arr[mid] < number){
-            return binarySearch(arr,mid + 1,stop,number);
+        if (arr[mid] < number) {
+            return binarySearch(arr, mid + 1, stop, number);
+        } else {
+            return binarySearch(arr, start, mid - 1, number);
         }
-        else{
-            return binarySearch(arr,start, mid - 1, number);
-        }
-    };
-
-    public static void main(String []s){
-        int arr[] = {2, 3, 4, 10, 40};
-        int x = 11;
-        System.out.println(new Array9().binarySearch(arr,0,4,10));
     }
 }
