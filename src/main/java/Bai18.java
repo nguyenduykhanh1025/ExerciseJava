91a66e259f0c588a590af7e8878ad9e5ddc7dd05
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-04 14:35:34 +0700

    update

--- src/main/java/Bai18.java
+++ src/main/java/Bai18.java
@@ -1,21 +1,11 @@
 public class Bai18 {
 
-    public int convertDecimalToBinaryNumber(int numberDecimal) {
+    public int covertDecimalToDecimalreserve(int numberDecimal) {
         int resultDecimalToBirary = 0, jumpValue = 0;
         while (numberDecimal != 0) {
             resultDecimalToBirary += Math.pow(10, jumpValue++) * (numberDecimal % 2);
             numberDecimal /= 2;
         }
-        return resultDecimalToBirary;
-    }
-
-    public int covertDecimalToDecimalreserve(int numberDecimal) {
-        int binaryNumber = convertDecimalToBinaryNumber(numberDecimal);
-        int lengthNumber = (int) Math.log10(binaryNumber), sumresult = 0;
-        while (lengthNumber >= 0) {
-            sumresult += (binaryNumber % 10 * Math.pow(2, lengthNumber--));
-            binaryNumber /= 10;
-        }
-        return sumresult;
+        return Integer.parseInt(new StringBuffer(String.valueOf(resultDecimalToBirary)).reverse().toString(), 2);
     }
 }
--- src/test/java/Bai18Test.java
+++ src/test/java/Bai18Test.java
@@ -5,11 +5,6 @@ import static org.junit.Assert.assertEquals;
 public class Bai18Test {
 
     @Test
-    public void test_convertDecimalToBinaryNumber(){
-        assertEquals(new Bai18().convertDecimalToBinaryNumber(123),1111011);
-    }
-
-    @Test
     public void test_covertDecimalToDecimalreserve(){
         assertEquals(new Bai18().covertDecimalToDecimalreserve(23),29);
     }
