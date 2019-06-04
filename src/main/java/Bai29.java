b3ce5be5b1c964c05d4f886cb6ced38b35fe6851
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-04 13:38:35 +0700

    udate Bai29

--- src/main/java/Bai29.java
+++ src/main/java/Bai29.java
@@ -1,29 +1,23 @@
+import java.util.HashMap;
+
 public class Bai29 {
 
-    public int getSingleDecimalNumber(char romanNumber) {
-        if (romanNumber == 'I')
-            return 1;
-        else if (romanNumber == 'V')
-            return 5;
-        else if (romanNumber == 'X')
-            return 10;
-        else if (romanNumber == 'L')
-            return 50;
-        else if (romanNumber == 'C')
-            return 100;
-        else if (romanNumber == 'D')
-            return 500;
-        else if (romanNumber == 'M')
-            return 1000;
-        return 0;
-    }
-
     public int coverRomanNumberToDecimalNumber(String romanNumber) {
+
+        HashMap<Character, Integer> hmRomanNumber = new HashMap<>();
+        hmRomanNumber.put('I', 1);
+        hmRomanNumber.put('V', 5);
+        hmRomanNumber.put('X', 10);
+        hmRomanNumber.put('L', 50);
+        hmRomanNumber.put('C', 100);
+        hmRomanNumber.put('D', 500);
+        hmRomanNumber.put('M', 1000);
+
         int lengtNumber = romanNumber.length();
-        int sumresult = getSingleDecimalNumber(romanNumber.charAt(lengtNumber - 1));
+        int sumresult = hmRomanNumber.get(romanNumber.charAt(lengtNumber - 1));
         int numberPrevious = sumresult;
         for (int i = romanNumber.length() - 2; i >= 0; --i) {
-            int numberCurent = getSingleDecimalNumber(romanNumber.charAt(i));
+            int numberCurent = hmRomanNumber.get(romanNumber.charAt(i));
             if (numberCurent < numberPrevious) {
                 sumresult -= numberCurent;
             } else {
--- src/test/java/Bai29Test.java
+++ src/test/java/Bai29Test.java
@@ -4,18 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 public class Bai29Test {
     @Test
-    public void test_getSingleDecimalNumber(){
-        assertEquals(new Bai29().getSingleDecimalNumber('I'),1);
-        assertEquals(new Bai29().getSingleDecimalNumber('V'),5);
-        assertEquals(new Bai29().getSingleDecimalNumber('X'),10);
-        assertEquals(new Bai29().getSingleDecimalNumber('L'),50);
-        assertEquals(new Bai29().getSingleDecimalNumber('C'),100);
-        assertEquals(new Bai29().getSingleDecimalNumber('D'),500);
-        assertEquals(new Bai29().getSingleDecimalNumber('M'),1000);
-        assertEquals(new Bai29().getSingleDecimalNumber(' '),0);
-    }
-
-    @Test
     public void test_coverRomanNumberToDecimalNumber(){
         assertEquals(new Bai29().coverRomanNumberToDecimalNumber("VI"),6);
         assertEquals(new Bai29().coverRomanNumberToDecimalNumber("LX"),60);
