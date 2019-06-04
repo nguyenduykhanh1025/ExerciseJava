87d7c3c79a19a2d59e25a787e148a197759f55ea
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-04 22:50:34 +0700

    code cu

--- src/main/java/Array11.java
+++ src/main/java/Array11.java
@@ -1,28 +1,38 @@
 public class Array11 {
-
     public int[] getArrayReverseEvenOdd(int[] arr, int n) {
-
         int indexOdd = n - 1, indexEven = n - 1;
-
         for (int i = 0; i <= n / 2; ++i) {
-            for (int j = (arr[i] % 2 == 0) ? indexEven : indexOdd; j >= 0; --j) {
-                if (arr[i] % 2 == 0 && arr[j] % 2 == 0 && j <= indexEven) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    indexEven = j - 1;
-                    break;
+            if (arr[i] % 2 == 0) {
+                for (int j = indexEven; j >= n / 2; --j) {
+                    if (arr[j] % 2 == 0) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        indexEven = j - 1;
+                        break;
+                    }
                 }
-                else if (arr[i] % 2 != 0 && arr[j] % 2 != 0 && j <= indexOdd) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    indexOdd = j - 1;
-                    break;
+            } else {
+                for (int j = indexOdd; j >= n / 2; --j) {
+                    if (arr[j] % 2 != 0) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        indexOdd = j - 1;
+                        break;
+                    }
                 }
             }
         }
         return arr;
     }
 
+    public static void main(String []s){
+        int []arr1= {2,2,4,5,6,7,8,10};
+        int n1 = 8;
+        int []result1 = new Array11().getArrayReverseEvenOdd(arr1,n1);
+        for(int i= 0; i< 8; ++i){
+            System.out.println(result1[i]);
+        }
+    }
 }
--- src/test/java/Array11Test.java
+++ src/test/java/Array11Test.java
@@ -10,5 +10,10 @@ public class Array11Test {
         int n = 8;
         int []result = {7,10,8,5,6,1,4,2};
         assertArrayEquals(new Array11().getArrayReverseEvenOdd(arr,n),result);
+
+        int []arr1= {2,2,4,5,6,7,8,10};
+        int n1 = 8;
+        int []result1 = {10,8,6,7,4,5,2,2};
+        assertArrayEquals(new Array11().getArrayReverseEvenOdd(arr1,n1),result1);
     }
 }
