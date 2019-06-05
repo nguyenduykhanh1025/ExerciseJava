866544b15c8de806d2ed8c929bb0bf0147e2342f
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-05 16:21:10 +0700

    up

--- src/main/java/Array13.java
+++ src/main/java/Array13.java
@@ -1,27 +1,14 @@
 public class Array13 {
 
-    public int[][] rotateArray90Degrees(int [][]arr, int n, int m){
-        int [][]result = new int[n][m];
-        for(int i = 0; i< n; ++i){
+    public int[][] rotateArray90Degrees(int[][] arr, int n, int m) {
+        int[][] result = new int[n][m];
+        for (int i = 0; i < n; ++i) {
             int k = m - 1;
-            for(int j = 0; j< m; ++j){
-                result[i][j]=arr[k][i];
+            for (int j = 0; j < m; ++j) {
+                result[i][j] = arr[k][i];
                 k--;
             }
         }
         return result;
-    }
-
-    public static void main(String []s){
-        int [][]arr = {{1,2},{3,4}};
-        int n = 2,m = 2;
-        int [][]result = new Array13().rotateArray90Degrees(arr,n,m);
-        for(int i= 0; i< n; ++i){
-            for(int j = 0; j<m; ++j){
-                System.out.print(result[i][j]  + " " );
-            }
-            System.out.println(" ");
-        }
-
     }
 }
