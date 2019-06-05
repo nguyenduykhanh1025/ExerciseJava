54bb0ee739ac76cebf56af9708561a05c4b33471
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-05 14:21:31 +0700

    up

--- src/main/java/Array6.java
+++ src/main/java/Array6.java
@@ -1,36 +1,17 @@
 public class Array6 {
 
-    public int[] insert(int[] arr, int indexValue, int indexInsert) {
-        int tempValue = arr[indexValue];
-        for (int i = indexValue; i > indexInsert; --i) {
-            arr[i] = arr[i - 1];
-        }
-        arr[indexInsert] = tempValue;
-        return arr;
-    }
-
-    public int[] sortArray(int[] arr, int n) {
-        for (int i = 0; i < n; ++i) {
-            if (arr[i] % 2 == 0) {
-                int indexInsert = -1;
-                for (int j = i - 1; j >= 0; --j) {
-                    if (arr[j] > arr[i] && arr[j] % 2 == 0) {
-                        indexInsert = j;
-                    }
+    public int[] sortArr(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j] && arr[i] % 2 != 0 && arr[j] % 2 != 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
-                if (indexInsert != -1) {
-                    arr = insert(arr, i, indexInsert);
-                }
-            }
-            if (arr[i] % 2 != 0) {
-                int indexInsert = -1;
-                for (int j = i - 1; j >= 0; --j) {
-                    if (arr[j] < arr[i] && arr[j] % 2 != 0) {
-                        indexInsert = j;
-                    }
-                }
-                if (indexInsert != -1) {
-                    arr = insert(arr, i, indexInsert);
+                if (arr[i] > arr[j] && arr[i] % 2 == 0 && arr[j] % 2 == 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
