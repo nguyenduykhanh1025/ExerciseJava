e17cffa2dec1e6ac8447ee5b9e9f4ec57fd46a72
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-05 13:59:48 +0700

    up

--- src/main/java/Bai18.java
+++ src/main/java/Bai18.java
@@ -1,11 +1,11 @@
 public class Bai18 {
 
     public int covertDecimalToDecimalreserve(int numberDecimal) {
-        int resultDecimalToBirary = 0, jumpValue = 0;
+        int resultDecimal = 0, jumpValue = 0;
         while (numberDecimal != 0) {
-            resultDecimalToBirary += Math.pow(10, jumpValue++) * (numberDecimal % 2);
-            numberDecimal /= 2;
+            resultDecimal = (resultDecimal << 1) | (numberDecimal & 1);
+            numberDecimal >>= 1;
         }
-        return Integer.parseInt(new StringBuffer(String.valueOf(resultDecimalToBirary)).reverse().toString(), 2);
+        return resultDecimal;
     }
 }
