e5145226f0073b21c83291dce3b48c6d7f781b51
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 22:00:26 +0700

    up

--- src/main/java/Array25.java
+++ src/main/java/Array25.java
@@ -13,17 +13,13 @@ public class Array25 {
         return findUCLN(a, b - a);
     }
 
-    public int findBCNN(int a, int b) {
-        return (a * b) / findUCLN(a, b);
-    }
 
     public int getUCNNArray(int[] arr) {
-        int result = 0;
-        int temp = findUCLN(arr[0], arr[1]);
+        int result = findUCLN(arr[0], arr[1]);
         for (int i = 2; i < arr.length; ++i) {
-            temp = findUCLN(temp, arr[i]);
+            result = findUCLN(result, arr[i]);
         }
-        return temp;
+        return result;
     }
 
 }
--- src/test/java/Array25Test.java
+++ src/test/java/Array25Test.java
@@ -7,16 +7,15 @@ public class Array25Test {
     public void test_getUCNNArray(){
         int []arr = {24,6,9};
         assertEquals(new Array25().getUCNNArray(arr),3);
+        int []arr1 = {0,2,1};
+        assertEquals(new Array25().getUCNNArray(arr1),1);
+
     }
     @Test
     public void test_findUCLN(){
-        assertEquals(new Bai1().findUCLN(42,56),14);
-        assertEquals(new Bai1().findUCLN(6,6),6);
-        assertEquals(new Bai1().findUCLN(56,42),14);
-        assertEquals(new Bai1().findUCLN(56,0),56);
-    }
-    @Test
-    public void test_findBCNN(){
-        assertEquals(new Bai1().findBCNN(4,6),12);
+        assertEquals(new Array25().findUCLN(42,56),14);
+        assertEquals(new Array25().findUCLN(6,6),6);
+        assertEquals(new Array25().findUCLN(56,42),14);
+        assertEquals(new Array25().findUCLN(56,0),56);
     }
 }
