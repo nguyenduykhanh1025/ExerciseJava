fb37e3c719c2a07542e3b2164d3c49ed4b560aab
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 13:54:34 +0700

    up

--- src/main/java/Array14.java
+++ src/main/java/Array14.java
@@ -1,18 +1,31 @@
 public class Array14 {
 
-    public int[] getArrayFromTwoDimensional(int[][] arr, int m, int n) {
+    public int[] getArrayFromTwoDimensional(int[][] arr) {
 
-        int length = n * m, index = 0;
+        int length = arr.length * arr[0].length;
+        int index = 0;
+
         int[] arrResult = new int[length];
-        int bottom = 0, right = 0, top = n - 1, left = m - 1;
+
+        int bottom = 0;
+        int right = 0;
+        int top = arr.length - 1;
+        int left = arr[0].length - 1;
+
         while (index < length) {
 
             for (int k = right; k <= left; k++) {
+                if (index >= length) {
+                    break;
+                }
                 arrResult[index++] = arr[bottom][k];
             }
             bottom++;
 
             for (int k = bottom; k <= top; k++) {
+                if (index >= length) {
+                    break;
+                }
                 arrResult[index++] = arr[k][left];
             }
             left--;
@@ -26,6 +39,9 @@ public class Array14 {
             top--;
 
             for (int k = top; k >= bottom; k--) {
+                if (index >= length) {
+                    break;
+                }
                 arrResult[index++] = arr[k][right];
             }
             right++;
@@ -33,5 +49,4 @@ public class Array14 {
         }
         return arrResult;
     }
-
 }
--- src/test/java/Array14Test.java
+++ src/test/java/Array14Test.java
@@ -6,9 +6,43 @@ import static org.junit.Assert.assertEquals;
 public class Array14Test {
     @Test
     public void test_getArrayFromTwoDimensional(){
-        int[][] arr = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
-        int m = 5, n = 3;
+        int[][] arr = {
+                {1,     2,      3,      4,      5},
+                {6,     7,      8,      9,      10},
+                {11,    12,     13,     14,     15}};
         int[] arrResult = {1,2,3,4,5,10,15,14,13,12,11,6,7,8,9};
-        assertArrayEquals(new Array14().getArrayFromTwoDimensional(arr,m,n),arrResult);
+        assertArrayEquals(new Array14().getArrayFromTwoDimensional(arr),arrResult);
+    }
+
+    @Test
+    public void test_getArrayFromTwoDimensional23(){
+        int[][] arr = {
+                {1},
+                {6},
+                {6},
+                {6},
+                {6},
+                {11}};
+        int[] arrResult = {1,6,6,6,6,11};
+        assertArrayEquals(new Array14().getArrayFromTwoDimensional(arr),arrResult);
+    }
+
+    @Test
+    public void test_getArrayFromTwoDimensional2(){
+        int[][] arr = {
+                {1,      5},
+                {6,      10},
+                {6,      10},
+                {11,     15}};
+        int[] arrResult = {1,5,10,10,15,11,6,6};
+        assertArrayEquals(new Array14().getArrayFromTwoDimensional(arr),arrResult);
+    }
+
+    @Test
+    public void test_getArrayFromTwoDimensional22(){
+        int[][] arr = {
+                {1}};
+        int[] arrResult = {1};
+        assertArrayEquals(new Array14().getArrayFromTwoDimensional(arr),arrResult);
     }
 }
