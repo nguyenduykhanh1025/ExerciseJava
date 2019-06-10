a3e227cae4ce96a75cc9a63f44df8ef1d5f5b33b
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 15:55:25 +0700

    test

--- src/test/java/Array19Test.java
+++ src/test/java/Array19Test.java
@@ -48,6 +48,15 @@ public class Array19Test {
     }
 
     @Test
+    public void test_sortMatrixFollowSpiral4() {
+        int[][] arr = {{1,2},{3,4},{5,6}};
+        int[][] arrResult = {{1,2},{6,3},{5,4}};
+        for (int i = 0; i < arr.length; ++i) {
+            assertArrayEquals(new Array19().sortMatrixFollowSpiral(arr)[i], arrResult[i]);
+        }
+    }
+
+    @Test
     public void test_sortMatrix() {
         int[][] arr = { {1, 2, 3, 4, 5},
                         {12, 13, 14, 15, 6},
