f80e6d98fa346f7b6c412b45b9ccbafd4251957d
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 15:11:08 +0700

    WIP

--- src/test/java/Array19Test.java
+++ src/test/java/Array19Test.java
@@ -57,10 +57,11 @@ public class Array19Test {
 
     @Test
     public void test_getMatrixSpiral2() {
-        int[][] arr = { {1, 2},
-                {6, 7},
-                {11, 12}};
-        int[][] arrResult = {{1,2},{12,6},{11,7}};
+        int[][] arr = {
+                {1,2,3,4},
+                {5,6,7,8}};
+        int[][] arrResult = {{1,2,3,4},
+                {8,7,6,5}};
         for (int i = 0; i < arr.length; ++i) {
             assertArrayEquals(new Array19().getMatrixSpiral(arr)[i], arrResult[i]);
         }
