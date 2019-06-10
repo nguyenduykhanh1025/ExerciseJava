313d1d8d80d9881e2bf61c9b9b9baef857fd9956
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 13:58:45 +0700

    up

--- src/main/java/Array18.java
+++ src/main/java/Array18.java
@@ -1,22 +1,23 @@
 public class Array18 {
 
-    public int[][] sortMatrix(int[][] arr, int n, int m) {
-
-        int lenght = n * m;
-        for (int i = 0; i < lenght - 1; i++) {
-            for (int j = i + 1; j < lenght; j++) {
-                if (arr[i / m][i % m] < arr[j / m][j % m]) {
-                    int temp = arr[j / m][j % m];
-                    arr[j / m][j % m] = arr[i / m][i % m];
-                    arr[i / m][i % m] = temp;
+    public int[][] sortMatrix(int[][] arr) {
+        int row = arr.length;
+        int column = arr[0].length;
+        int length = row * column;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (arr[i / column][i % column] < arr[j / column][j % column]) {
+                    int temp = arr[j / column][j % column];
+                    arr[j / column][j % column] = arr[i / column][i % column];
+                    arr[i / column][i % column] = temp;
                 }
             }
         }
 
-        for (int i = 0; i <= n / 2; ++i) {
+        for (int i = 0; i <= row / 2; ++i) {
             int[] temp = arr[i];
-            arr[i] = arr[n - i - 1];
-            arr[n - i - 1] = temp;
+            arr[i] = arr[row - i - 1];
+            arr[row - i - 1] = temp;
         }
         return arr;
     }
--- src/test/java/Array18Test.java
+++ src/test/java/Array18Test.java
@@ -8,9 +8,9 @@ public class Array18Test {
     public void test_sortMatrix(){
         int [][] arr = {{1,2,3,4,5},{16,17,18,19,6},{15,24,25,20,7},{14,23,22,21,8},{13,12,11,10,9}};
         int [][] arrResult = {{5,4,3,2,1},{10,9,8,7,6},{15,14,13,12,11},{20,19,18,17,16},{25,24,23,22,21}};
-        int n = 5, m = 5;
+        int n = 5;
         for(int i = 0; i< n; ++i){
-            assertArrayEquals(new Array18().sortMatrix(arr,n,m)[i],arrResult[i]);
+            assertArrayEquals(new Array18().sortMatrix(arr)[i],arrResult[i]);
         }
     }
 }
