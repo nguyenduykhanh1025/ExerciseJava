441bd04b58abbcc4e81fff02004c33eef521553b
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 14:10:34 +0700

    test

--- src/test/java/Array14Test.java
+++ src/test/java/Array14Test.java
@@ -41,10 +41,8 @@ public class Array14Test {
     @Test
     public void test_getArrayFromTwoDimensional22(){
         int[][] arr = {
-                {1,2},
-                {3,4},
-                {5,6}};
-        int[] arrResult = {1,2,4,6,5,3};
+                {1,2,3,4}};
+        int[] arrResult = {1,2,3,4};
         assertArrayEquals(new Array14().getArrayFromTwoDimensional(arr),arrResult);
     }
 }
