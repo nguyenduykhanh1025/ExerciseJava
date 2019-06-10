70609c26907fea33f01eccf6acdac218ebb81005
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 13:16:36 +0700

    up

--- src/main/java/Array12.java
+++ src/main/java/Array12.java
@@ -11,20 +11,15 @@ public class Array12 {
         return arrTemp;
     }
 
-    public int[] getInsertArrayAscending(int[] arr, int value, int start, int stop) {
-        if (arr[arr.length - 1] < value) {
-            return getInsert(arr, arr.length, value);
+    public int[] getInsertArrayAscending(int[] arr, int value) {
+        int lenght = arr.length;
+        int[] arrResult = new int[lenght + 1];
+        for (int i = 0; i < lenght; ++i) {
+            if (arr[i] > value) {
+                arrResult = getInsert(arr, i, value);
+                break;
+            }
         }
-        if (arr[0] > value) {
-            return getInsert(arr, 0, value);
-        }
-        int mid = (start + stop) / 2;
-        if (value > arr[mid + 1]) {
-            return getInsertArrayAscending(arr, value, mid, stop);
-        } else if (value < arr[mid]) {
-            return getInsertArrayAscending(arr, value, start, mid);
-        } else {
-            return getInsert(arr, mid + 1, value);
-        }
+        return arrResult;
     }
 }
--- src/test/java/Array12Test.java
+++ src/test/java/Array12Test.java
@@ -5,27 +5,17 @@ import static org.junit.Assert.assertEquals;
 
 public class Array12Test {
     @Test
-    public void test_getInsert(){
-        int []arr = {1,2,4,5};
-        int []arrResult = {1,2,3,4,5};
-        assertArrayEquals(new Array12().getInsert(arr,2,3),arrResult);
+    public void test_getInsert() {
+        int[] arr = {1, 2, 4, 5};
+        int[] arrResult = {1, 2, 3, 4, 5};
+        assertArrayEquals(new Array12().getInsert(arr, 2, 3), arrResult);
     }
 
     @Test
-    public void test_getInsertArrayAscending(){
-        int []arr = {1,2,4,5};
-        int []arrResult = {1,2,3,4,5};
-        int []arrResult1 = {-1,1,2,4,5};
-        int []arrResult2 = {1,2,4,5,6};
-        int []arrResult3 = {1,1,2,4,5};
-
-        assertArrayEquals(new Array12().getInsertArrayAscending(arr,3,0,4), arrResult);
-        assertArrayEquals(new Array12().getInsertArrayAscending(arr,-1,0,4), arrResult1);
-        assertArrayEquals(new Array12().getInsertArrayAscending(arr,6,0,4), arrResult2);
-        assertArrayEquals(new Array12().getInsertArrayAscending(arr,1,0,4), arrResult3);
-        int []arr1 = {1,2,3,4,6};
-        int []arrResult4 = {1,2,3,4,5,6};
-        assertArrayEquals(new Array12().getInsertArrayAscending(arr1,5,0,5), arrResult4);
+    public void test_getInsertArrayAscending() {
+        int[] arr = {1, 2, 4, 5};
+        int[] arrResult = {1, 2, 3, 4, 5};
+        assertArrayEquals(new Array12().getInsertArrayAscending(arr, 3), arrResult);
     }
 
 }
