dfaa5c57e7bc03cf13b4ccb5e2c9d8b3094a7cf6
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-10 14:50:33 +0700

    up

---
+++ src/main/java/Array19.java
@@ -0,0 +1,94 @@
+public class Array19 {
+
+    public int[][] sortMatrix(int[][] arr) {
+        int row = arr.length;
+        int column = arr[0].length;
+        int length = row * column;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (arr[i / column][i % column] > arr[j / column][j % column]) {
+                    int temp = arr[j / column][j % column];
+                    arr[j / column][j % column] = arr[i / column][i % column];
+                    arr[i / column][i % column] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public int[][] getMatrixSpiral(int[][] arr) {
+        int row = arr.length;
+        int column = arr[0].length;
+        int length = row*column;
+        int indexX = 0;
+        int indexY = 0;
+
+        int[][] arrResult = new int[row][column];
+
+        int bottom = 0;
+        int right = 0;
+        int top = row - 1;
+        int left = column - 1;
+
+
+        while (indexX < row) {
+
+            for (int k = right; k <= left; k++) {
+                arrResult[bottom][k] = arr[indexX][indexY++];
+                if (indexY == column) {
+                    indexX++;
+                    indexY = 0;
+                }
+            }
+            bottom++;
+
+            for (int k = bottom; k <= top; k++) {
+                arrResult[k][left] = arr[indexX][indexY++];
+                if (indexY == column) {
+                    indexX++;
+                    indexY = 0;
+                }
+            }
+            left--;
+
+            for (int k = left; k >= right; k--) {
+                if (indexX >= row) {
+                    break;
+                }
+                arrResult[top][k] = arr[indexX][indexY++];
+                if (indexY == column) {
+                    indexX++;
+                    indexY = 0;
+                }
+            }
+            top--;
+            for (int k = top; k >= bottom; k--) {
+                if (indexX >= row) {
+                    break;
+                }
+                arrResult[k][right] = arr[indexX][indexY++];
+                if (indexY == column) {
+                    indexX++;
+                    indexY = 0;
+                }
+            }
+            right++;
+        }
+        return arrResult;
+    }
+
+    public int[][] sortMatrixFollowSpiral(int arr[][]) {
+        return getMatrixSpiral(sortMatrix(arr));
+    }
+
+    public static void main(String[] s) {
+        int[][] arr = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+        arr = new Array19().getMatrixSpiral(arr);
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ src/test/java/Array19Test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+public class Array19Test {
+    @Test
+    public void test_sortMatrix() {
+        int[][] arr = { {1, 2, 3, 4, 5},
+                        {6, 7, 8, 9, 10},
+                        {11, 12, 13, 14, 15} };
+        int[][] arrResult = {   {1, 2, 3, 4, 5},
+                                {12, 13, 14, 15, 6},
+                                {11, 10, 9, 8, 7}   };
+        for(int i = 0; i< arr.length;++i){
+            assertArrayEquals(new Array19().sortMatrixFollowSpiral(arr)[i],arrResult[i]);
+        }
+    }
+}
