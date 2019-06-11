332aae147c60d143f64effaeafa36b10de45fdd1
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-11 21:25:11 +0700

    up

--- src/main/java/Array21.java
+++ src/main/java/Array21.java
@@ -1,38 +1,51 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 public class Array21 {
 
-    public int[][] getSubMatrix(int[][] arr, int xRow, int yColunm) {
-        int[][] arrResult = new int[arr.length - 1][arr[0].length - 1];
+    public int[][] getSubMatrix(int[][] arr, int indexRowDelete, int indexColumnDelete) {
+        int row = arr.length;
+        int column = arr[0].length;
+        int[][] arrResult = new int[row - 1][column - 1];
+        int[][] arrTemp = new int[arr.length][arr[0].length];
+
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = 0; j < arr[0].length; ++j) {
+                arrTemp[i][j] = arr[i][j];
+            }
+        }
+
+        for (int i = indexRowDelete; i < row - 1; ++i) {
+            arrTemp[i] = arrTemp[i + 1];
+        }
+        for (int i = indexColumnDelete; i < column - 1; ++i) {
+            for (int j = 0; j < row - 1; ++j) {
+                arrTemp[j][i] = arrTemp[j][i + 1];
+            }
+        }
+        for (int i = 0; i < row - 1; ++i) {
+            for (int j = 0; j < column - 1; ++j) {
+                arrResult[i][j] = arrTemp[i][j];
+            }
+        }
+
         return arrResult;
     }
-    public int check(int n){
-        if(n % 2 == 0){
-            return 1;
-        }
-        return -1;
-    }
-    public int getDeterminantMatrix(float[][] arr) {
-        for(int i = 0; i< arr.length; ++i){
-            return check(i)*arr[i][0]*getDeterminantMatrix(getSubMatrix())
-        }
-    }
 
-    public static void main(String []s){
-        int[][] arr = {
-                {4, 11, 28, 3, 9, 1},
-                {2, 2, 4, 28, 2, 14},
-                {6, 3, 9, 17, 18, 7},
-                {3, 3, 1, 16, 6, 15},
-                {6, 5, 3, 2, 9, 23},
-                {1, 6, 11, 2, 19, 16}
-        };
-        int [][]arrResult = new Array21().getSubMatrix(arr,1,1,2,2);
-        //System.out.println(new Array21().getDeterminantMatrix(arr));
-        for(int i = 0; i< arrResult.length; ++i){
-            for(int j = 0; j< arrResult[0].length; ++j){
-                System.out.print(arrResult[i][j] + "  ");
-            }
-            System.out.println("");
+    public int getDeterminantMatrix(int[][] arr) {
+        int n = arr.length;
+        int s, sum = 0;
+        if (n == 1) {
+            return arr[0][0];
         }
+        for (int k = 0; k < n; k++) {
+            if (k % 2 == 0) {
+                s = 1;
+            } else {
+                s = -1;
+            }
+            sum += arr[k][0] * s * getDeterminantMatrix(getSubMatrix(arr, k, 0));
+        }
+        return sum;
     }
 
 }
--- src/test/java/Array21Test.java
+++ src/test/java/Array21Test.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class Array21Test {
     @Test
     public void test_getDeterminantMatrix2() {
-        float[][] arr = {
+        int[][] arr = {
                 {1, 9, 3, 4},
                 {5, 6, 7, 9},
                 {3, 10, 11, 12},
@@ -18,7 +18,7 @@ public class Array21Test {
 
     @Test
     public void test_getDeterminantMatrix3() {
-        float[][] arr = {
+        int[][] arr = {
                 {4, 11, 28, 3, 9, 1},
                 {2, 2, 4, 28, 2, 14},
                 {6, 3, 9, 17, 18, 7},
@@ -32,7 +32,7 @@ public class Array21Test {
 
     @Test
     public void test_getDeterminantMatrix4() {
-        float[][] arr = {
+        int[][] arr = {
                 {30, 2, 69, 35, 66},
                 {8, 32, 52, 59, 11},
                 {87, 41, 97, 39, 79},
@@ -45,7 +45,7 @@ public class Array21Test {
 
     @Test
     public void test_getDeterminantMatrix5() {
-        float[][] arr = {
+        int[][] arr = {
                 {76, 99, 85},
                 {54, 25, 56},
                 {33, 9, 73}
