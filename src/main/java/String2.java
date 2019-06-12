3b0dd336c995718377c2ad21a459ee2cbdab5379
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-12 10:21:21 +0700

    up

--- src/main/java/String2.java
+++ src/main/java/String2.java
@@ -6,12 +6,7 @@ public class String2 {
         ArrayList<String> listResult = new ArrayList<>();
         for(int i = 0; i< str.length(); ++i){
             char dataTemp = str.charAt(i);
-            if ((dataTemp >= 97 && dataTemp <= 122)) {
-                System.out.println("co");
-                listResult.add(String.valueOf(dataTemp));
-            }
-            if(dataTemp >= 48 && dataTemp <= 57){
-                System.out.println("khong");
+            if ((dataTemp >= 97 && dataTemp <= 122) || dataTemp >= 48 && dataTemp <= 57) {
                 listResult.add(String.valueOf(dataTemp));
             }
         }
