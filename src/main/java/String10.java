06204f7ecea6cb3a0d682924e228b801b983a968
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-12 17:00:09 +0700

    up

--- src/main/java/String10.java
+++ src/main/java/String10.java
@@ -48,4 +48,5 @@ public class String10 {
 
         return strResult.toString();
     }
+
 }
--- src/test/java/String10Test.java
+++ src/test/java/String10Test.java
@@ -10,6 +10,12 @@ public class String10Test {
     }
 
     @Test
+    public void test_getRevivalString2(){
+        String s = "a12+2";
+        assertEquals(new String10().getRevivalString(s),"aaaaaaaaaaaa++");
+    }
+
+    @Test
     public void test_shortcutString(){
         String str = "abcccceeeefddd";
         String result = "abc4e4fd3";
