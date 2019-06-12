ae2ad6f07a9e4b50cce88790fce6f5a5003aea3c
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-12 10:08:51 +0700

    up

--- src/test/java/String2Test.java
+++ src/test/java/String2Test.java
@@ -8,8 +8,8 @@ import static org.junit.Assert.assertEquals;
 public class String2Test {
     @Test
     public void test_getCharInto(){
-        String str = "abcA01*7";
-        ArrayList<String> list = new ArrayList<String>(Arrays.asList("a","b","c","0","1","7"));
+        String str = "abcA01*79z";
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("a","b","c","0","1","7","9","z"));
         assertEquals(new String2().getCharInto(str),list);
     }
 }
