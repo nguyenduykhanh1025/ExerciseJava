a15b32ec7dde6692a95e17e6b469c9a2bba29266
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-13 16:38:50 +0700

    up

--- src/main/java/String16.java
+++ src/main/java/String16.java
@@ -1,9 +1,9 @@
 public class String16 {
 
 
-    public int getSum(String a, String b){
+    public int getSum(String a, String b) {
 
-        if(a.length() > b.length()){
+        if (a.length() > b.length()) {
             String temp = a;
             a = b;
             b = temp;
@@ -19,7 +19,7 @@ public class String16 {
         int numberA = 0;
         int sum = 0;
 
-        for(indexA = a.length() - 1;indexA >= 0;--indexA){
+        for (indexA = a.length() - 1; indexA >= 0; --indexA) {
 
             numberA = a.charAt(indexA) - 48;
             numberB = b.charAt(indexB) - 48;
@@ -36,15 +36,7 @@ public class String16 {
         sum = surplus == 1 ? numberB + 1 : numberB;
         strResult.append(sum % 10);
 
-        return Integer.parseInt(strResult.append(b.substring(0,indexB + 1)).reverse().toString());
-    }
-
-
-
-    public static void main(String []s){
-        String a = "423";
-        String b = "1456";
-        System.out.println(new String16().getSum(a,b));
+        return Integer.parseInt(strResult.append(b.substring(0, indexB + 1)).reverse().toString());
     }
 
 }
