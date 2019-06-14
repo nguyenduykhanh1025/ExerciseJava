95c97ec0392757d98bd7c0e6123bfdf9c58c0853
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-14 15:12:53 +0700

    up

--- src/main/java/String7.java
+++ src/main/java/String7.java
@@ -3,17 +3,14 @@ public class String7 {
     public int getIndexSubString(String str, String subStr) {
         int indexSubStr;
         for (int i = 0; i < str.length(); ++i) {
-            indexSubStr = 0;
-            if (str.charAt(i) == subStr.charAt(0)) {
-                int j = i + 1;
-                for (indexSubStr = 1; indexSubStr < subStr.length() && j < str.length(); ++indexSubStr) {
-                    if (str.charAt(j++) != subStr.charAt(indexSubStr)) {
-                        break;
-                    }
+            int j = ++i;
+            for (indexSubStr = 0; indexSubStr < subStr.length() && j < str.length(); ++indexSubStr) {
+                if (str.charAt(j++) != subStr.charAt(indexSubStr)) {
+                    break;
                 }
-                if (indexSubStr == subStr.length()) {
-                    return i;
-                }
+            }
+            if (indexSubStr == subStr.length()) {
+                return i;
             }
         }
         return -1;
