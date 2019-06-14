e11cfa0d3e56290d410c2efb435d27e0c02f5fd2
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-14 16:06:12 +0700

    string-19

---
+++ src/main/java/String19.java
@@ -0,0 +1,74 @@
+public class String19 {
+    public String addZero(String a, int countZero) {
+        StringBuffer strATemp = new StringBuffer(a);
+        for (int i = 0; i < countZero; ++i) {
+            a = strATemp.insert(0, '0').toString();
+            strATemp = new StringBuffer(a);
+        }
+        return a;
+    }
+
+
+    public String getSubtraction(String a, String b) {
+
+        int instance = a.length() - b.length();
+        boolean checkSign = true;
+        if (instance < 0) {
+            a = addZero(a, -instance);
+        }
+        if (instance > 0) {
+            b = addZero(b, instance);
+        }
+
+        if (a.compareTo(b) < 0) {
+            String temp = a;
+            a = b;
+            b = temp;
+            checkSign = false;
+        }
+
+        StringBuffer strResult = new StringBuffer();
+
+        int indexA = a.length() - 1;
+        int indexB;
+
+
+        int numberB = 0;
+        int numberA = 0;
+        int surplus = 0;
+        int subtract = 0;
+
+        for (indexB = b.length() - 1; indexB >= 0; --indexB) {
+            numberA = a.charAt(indexA) - '0';
+            numberB = b.charAt(indexB) - '0';
+
+            if (numberA - numberB - surplus >= 0) {
+                subtract = numberA - numberB - surplus;
+            } else {
+                subtract = 10 + numberA - numberB - surplus;
+            }
+            surplus = numberA - numberB - surplus >= 0 ? 0 : 1;
+
+            strResult.append(Math.abs(subtract));
+            indexA--;
+
+        }
+
+        return !checkSign ? strResult.reverse().insert(0, '-').toString() : strResult.reverse().toString();
+    }
+
+    public int getDivision(String a, String b) {
+
+        if (a.length() < b.length() || (a.length() == b.length() && a.compareTo(b) < 0)) {
+            return 0;
+        }
+
+        int result = 0;
+        while (!getSubtraction(a, b).equals("0") && !getSubtraction(a, b).contains("-")) {
+            a = getSubtraction(a, b);
+            result++;
+        }
+
+        return result;
+    }
+}
