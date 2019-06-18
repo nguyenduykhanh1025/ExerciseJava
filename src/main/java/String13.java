503e5d255e53686399a81b2d71220758f35a8521
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-18 10:32:44 +0700

    up

--- src/main/java/String13.java
+++ src/main/java/String13.java
@@ -2,13 +2,12 @@ public class String13 {
 
     public boolean isSameString(String strA, String strB) {
 
-        if(strA.length() != strB.length()){
+        if (strA.length() != strB.length()) {
             return false;
         }
 
         strA = strA.toLowerCase();
         strB = strB.toLowerCase();
-
 
         for (int i = 0; i < strA.length(); ++i) {
 
