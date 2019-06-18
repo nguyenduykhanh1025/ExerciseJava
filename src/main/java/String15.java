02f70142848f6ddfe850855039fc294037cc6843
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-18 14:18:30 +0700

    up

--- src/main/java/String15.java
+++ src/main/java/String15.java
@@ -4,23 +4,10 @@ public class String15 {
 
         int sum = 0;
 
-        int i = 0;
-        for (i = 0; i < strData.length(); ++i) {
-            sum += (1 + 2* (i & 1))* (strData.charAt(i) - '0');
+        for (int i = 0; i < strData.length(); ++i) {
+            sum += (1 + 2 * (i & 1)) * (strData.charAt(i) - '0');
         }
 
-
-        for (int j = 0; j <= 9; ++j) {
-            if (i % 2 == 0) {
-                if ((sum + j) % 10 == 0) {
-                    return j++;
-                }
-            } else {
-                if ((sum + j / 3) % 10 == 0) {
-                    return j;
-                }
-            }
-        }
-        return -1;
+        return (10 - sum % 10) % 10;
     }
 }
--- src/test/java/String15Test.java
+++ src/test/java/String15Test.java
@@ -7,22 +7,16 @@ import static org.junit.Assert.assertEquals;
 public class String15Test {
     @Test
     public void test_getVerifyNumber(){
-        String str = "893850597419";
-        int result = 4;
-        assertEquals(new String15().getVerifyNumber(str),result);
+        assertEquals(new String15().getVerifyNumber("893850597419"),4);
     }
 
     @Test
     public void test_getVerifyNumber1(){
-        String str = "8938505974195";
-        int result = -1;
-        assertEquals(new String15().getVerifyNumber(str),result);
+        assertEquals(new String15().getVerifyNumber("893502532933"),6);
     }
 
     @Test
     public void test_getVerifyNumber2(){
-        String str = "8938505974194";
-        int result = 0;
-        assertEquals(new String15().getVerifyNumber(str),result);
+        assertEquals(new String15().getVerifyNumber("893850597419"),4);
     }
 }
