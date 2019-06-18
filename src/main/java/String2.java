52947d36177de2e7dc4213a75388afe07c4bef13
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-18 09:34:29 +0700

    up

--- src/main/java/String2.java
+++ src/main/java/String2.java
@@ -2,15 +2,14 @@ import java.util.ArrayList;
 
 public class String2 {
 
-    public ArrayList<String> getCharInto(String str){
+    public ArrayList<String> getCharInto(String str) {
         ArrayList<String> listResult = new ArrayList<>();
-        for(int i = 0; i< str.length(); ++i){
+        for (int i = 0; i < str.length(); ++i) {
             char dataTemp = str.charAt(i);
-            if ((dataTemp >= 97 && dataTemp <= 122) || dataTemp >= 48 && dataTemp <= 57) {
+            if (Character.isLetterOrDigit(dataTemp) && !Character.isUpperCase(dataTemp)) {
                 listResult.add(String.valueOf(dataTemp));
             }
         }
         return listResult;
-    }
 
-}
+    }
