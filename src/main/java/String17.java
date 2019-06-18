055fd1a69a957aea8b4f777dae1213ec260e4486
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-18 11:44:23 +0700

    up

--- src/main/java/String17.java
+++ src/main/java/String17.java
@@ -17,7 +17,7 @@ public class String17 {
 
         if (instance < 0) {
             a = addZero(a, -instance);
-        }else {
+        } else {
             b = addZero(b, instance);
         }
 
@@ -33,31 +33,27 @@ public class String17 {
         int indexA = a.length() - 1;
         int indexB;
 
-
-        int numberB;
-        int numberA;
-
         int surplus = 0;
         int subtract;
 
         for (indexB = b.length() - 1; indexB >= 0; --indexB) {
 
-            numberA = a.charAt(indexA) - '0';
-            numberB = b.charAt(indexB) - '0';
+            int subtractTemp = (a.charAt(indexA) - '0') - (b.charAt(indexB) - '0') - surplus;
 
-            if( numberA - numberB - surplus >= 0){
-                subtract = numberA - numberB - surplus;
-            }else{
-                subtract = 10 + numberA - numberB - surplus;
+            if (subtractTemp >= 0) {
+                subtract = subtractTemp;
+            } else {
+                subtract = 10 + subtractTemp;
             }
 
-            surplus = numberA - numberB - surplus >= 0 ? 0 : 1;
+            surplus = subtractTemp >= 0 ? 0 : 1;
 
-            strResult.insert(0,Math.abs(subtract));
+            strResult.insert(0, Math.abs(subtract));
             indexA--;
 
         }
 
-        return !checkSign ? strResult.insert(0,'-').toString() : strResult.toString();
+        return !checkSign ? "-" + strResult.toString() : strResult.toString();
     }
+
 }
