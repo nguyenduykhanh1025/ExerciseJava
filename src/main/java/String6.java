0aa4dd873aaf649305f88203b7aa20b4c3068297
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-18 09:46:59 +0700

    up

--- src/main/java/String6.java
+++ src/main/java/String6.java
@@ -1,15 +1,17 @@
 public class String6 {
 
     public String getCovert(String str) {
-        StringBuffer strResult = new StringBuffer("");
-        for (int i = 0; i < str.length(); ++i) {
-            if (str.charAt(i) < 91) {
-                strResult.append((char) (str.charAt(i) + 32));
-            } else {
-                strResult.append((char) (str.charAt(i) - 32));
-            }
 
+        StringBuilder strResult = new StringBuilder();
+
+        for (char ch: str.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                strResult.append(Character.toUpperCase(ch));
+            } else {
+                strResult.append(Character.toLowerCase(ch));
+            }
         }
+
         return strResult.toString();
     }
 }
--- src/test/java/String6Test.java
+++ src/test/java/String6Test.java
@@ -3,10 +3,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class String6Test {
+
     @Test
     public void test_getCovert(){
         String str = "ABCdef";
         String strResult = "abcDEF";
         assertEquals(new String6().getCovert(str),strResult);
     }
+
 }
