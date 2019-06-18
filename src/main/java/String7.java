e0fa37a70da59b464121c0cd592c88e46a789c6b
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-18 10:07:50 +0700

    up

--- src/main/java/String7.java
+++ src/main/java/String7.java
@@ -2,17 +2,25 @@ public class String7 {
 
     public int getIndexSubString(String str, String subStr) {
         int indexSubStr;
+
         for (int i = 0; i < str.length(); ++i) {
+
             int j = ++i;
+
             for (indexSubStr = 0; indexSubStr < subStr.length() && j < str.length(); ++indexSubStr) {
+
                 if (str.charAt(j++) != subStr.charAt(indexSubStr)) {
                     break;
                 }
+
             }
+
             if (indexSubStr == subStr.length()) {
                 return i;
             }
+
         }
+
         return -1;
     }
 }
