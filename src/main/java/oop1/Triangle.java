8024712ff7f4c893cb3c7f616a3976dff7c1531c
nguyenduykhanh1025 <nguyenduykhanh1025@gmail.com>
2019-06-19 10:34:08 +0700

    up

--- src/main/java/oop1/Triangle.java
+++ src/main/java/oop1/Triangle.java
@@ -2,44 +2,40 @@ package oop1;
 
 public class Triangle {
 
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
-    private int x3;
-    private int y3;
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+    private double x3;
+    private double y3;
 
     public double getLengthDistance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
-    public String getType() {
+    public typeShape getType() {
         double a = getLengthDistance(x1, y1, x2, y2);
         double b = getLengthDistance(x1, y1, x3, y3);
         double c = getLengthDistance(x3, y3, x2, y2);
-        StringBuffer strResult = new StringBuffer("khong phai la tam giac");
         if ((a + b) > c && (b + c) > a && (a + c) > b) {
-            strResult = new StringBuffer("");
 
-
-            if (Math.sqrt(a * a + b * b) == c || Math.sqrt(b * b + c * c) == a || Math.sqrt(a * a + c * c) == b) {
-                strResult.append("vuong");
+            if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
+                return typeShape.RIGHT_TRIANGLE;
             } else if (a == b || a == c || b == c) {
-                strResult.append("can");
+                return typeShape.ISOSCELES_TRIANGLE;
             } else {
-                strResult.append("thuong");
+                return typeShape.NORMAL;
             }
         }
-        return strResult.toString();
+        return typeShape.INVALID;
     }
 
     public double getPerimeter() {
         double a = getLengthDistance(x1, y1, x2, y2);
         double b = getLengthDistance(x1, y1, x3, y3);
         double c = getLengthDistance(x3, y3, x2, y2);
-        double S;
 
-        if (!this.getType().equals("khong phai la tam giac")) {
+        if (this.getType() != typeShape.INVALID) {
             return a + b + c;
         }
         return -1;
@@ -49,16 +45,14 @@ public class Triangle {
         double a = getLengthDistance(x1, y1, x2, y2);
         double b = getLengthDistance(x1, y1, x3, y3);
         double c = getLengthDistance(x3, y3, x2, y2);
-        double S;
         double p = getPerimeter();
         if (p != -1) {
-            S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-            return S;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
         }
         return -1;
     }
 
-    public Triangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
